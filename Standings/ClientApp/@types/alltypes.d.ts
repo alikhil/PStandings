1e250852568d921bc87253bd847fa852dd140178
Alik Khilazhev <alikhil@mail.ru>
2017-12-20 12:49:38 +0300

    build(package.json): Add chart.js dependency

--- Standings/ClientApp/@types/alltypes.d.ts
+++ Standings/ClientApp/@types/alltypes.d.ts
@@ -1,16 +1,2 @@
-// import { Component } from "react";
-
-// const rst = require('react-sticky-table');
-
-// export declare module 'react-sticky-table' {
-//     interface Cell extends React.Component{}
-//     interface Row extends React.Component {}
-//     interface StickyTable extends React.Component {}
-// }
-
 declare module 'react-sticky-table';
-// {
-//     interface Cell extends React.Component<{}, {}> {}
-//     interface Row extends  React.Component<{}, {}> {}
-//     interface StickyTable extends  React.Component<{}, {}> {}
-// }
+declare module 'chart.js';
