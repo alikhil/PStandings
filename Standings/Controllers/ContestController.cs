01ac427db63e909a1d8df82e71acdfff9a6912de
Alik Khilazhev <alikhil@mail.ru>
2018-01-12 23:08:23 +0300

    refactor(ContestController): Fix spaces

--- Standings/Controllers/ContestController.cs
+++ Standings/Controllers/ContestController.cs
@@ -186,8 +186,8 @@ namespace Standings.Controllers
         public static DateTime JavaTimeStampToDateTime( double javaTimeStamp )
         {
             // Java timestamp is milliseconds past epoch
-            System.DateTime dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddMilliseconds( javaTimeStamp ).ToLocalTime();
+            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddMilliseconds(javaTimeStamp).ToLocalTime();
             return dtDateTime;
         }
     }
